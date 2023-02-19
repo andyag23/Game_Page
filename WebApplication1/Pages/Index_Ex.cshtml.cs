d669822f71d5dee8e2557629e532548bd4cea3b9
Stanleykao <s9303358@gmail.com>
2023-02-19 17:51:20 +0800

    0219 調整初版

---
+++ WebApplication1/Pages/Index_Ex.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Pages
+{
+    public class Index_ExModel : PageModel
+    {
+        private readonly ILogger<Index_ExModel> _logger;
+
+        public Index_ExModel(ILogger<Index_ExModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
